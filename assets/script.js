43e66086e861072e6e7ab0670631c0c7dd8faab2
stevensillsii <stevensills2@gmail.com>
2023-10-08 23:30:35 -0700

    getting API key to work

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,44 @@
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
+const locationButton = document.querySelector(".location-btn");
 // API Key from OpenWeatherMap
-const API_KEY = "483485cb74d86bde93ff36e3ccd53cf5";
+const API_KEY = "3c55973fc99b3e92dab73b10e74cd9ac";
 
 const getWeatherDetails = (cityName, lat, lon) => {
-    const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&appid={API_KEY}';
+    const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}';
 
     fetch(WEATHER_API_URL).then(res => res.jason()).then(data => {
         console.log(data);
+
+        // so that we only see one forecast per day
+        const uniqueForecastDays = [];
+        const fiveDaysForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecast.dt.txt).getDate();
+            if(!uniqueForecastDays.includes(forecastDate)) {
+                return uniqueForecastDays.push(forecastDate);
+            }
+        });
+
+
     }).catch(() => {
         alert("An Error Occured While Predicting The Future About The Weather!");
-    })
+    });
 }
 
-const getCityCoodinates = () => {
+const getCityCoordinates = () => {
     const cityName = cityInput.value.trim();  
-    if(!cityName) return; 
+    if(cityName === "") return; 
 
     // This gets the city coodinates in latitude and longitude format with the city name from the API response.
-    const GEOCODING_API_URL = 'http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}'
+    const GEOCODING_API_URL = 'https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}';
+
     fetch(GEOCODING_API_URL).then(res => res.jason()).then(data => {
-        if(data.length) return alert('No directions to ${cityName} found!');
-        const { name, lat, lon } = data = [0];
-        getWeatherDetails();
+        if(!data.length) return alert('No directions to that city found!');
+        const { name, lat, lon } = data[0];
+        getWeatherDetails(name, lat, lon);
     }).catch(() => {
-        alert("An Error Occured While Getting The Directions To That City!")
+        alert("An Error Occured While Getting The Directions To That City!");
     });
 } 
 
-searchButton.addEventListener("click", getCityCoodinates);
+searchButton.addEventListener("click", getCityCoordinates);
