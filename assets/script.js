a4303268377bfafd1dc80a978ddc067bad2f3829
stevensillsii <stevensills2@gmail.com>
2023-10-09 22:18:24 -0700

    working on JS focusing on dynamically updating cards

--- assets/script.js
+++ assets/script.js
@@ -97,6 +97,44 @@ const getUserCoordinates = () => {
             }
         });
 }
+// add on click event listener 
+$(".search-btn").on("click", function(event) {
+    event.preventDefault();
+    console.log();
+
+    var city = $(".city-input").val().trim();
+    getWeatherDetails(cityName);
+    if (!searchHistoryList.includes(cityName)) {
+        searchHistoryList.push(cityName);
+        var searchedCity = $(`
+            <li class="list-group-item">${cityName}</li>
+            `);
+        $("#searchHistory").append(searchedCity);
+    };
+    
+    localStorage.setItem("city", JSON.stringify(searchHistoryList));
+    console.log(searchHistoryList);
+});
+
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+$(document).on("click", ".list-group-item", function() {
+    var listCity = $(this).text();
+    getWeatherDetails(listCity);
+});
+
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast
+$(document).ready(function() {
+    var searchHistoryArr = JSON.parse(localStorage.getItem("city"));
+
+    if (searchHistoryArr !== null) {
+        var lastSearchedIndex = searchHistoryArr.length - 1;
+        var lastSearchedCity = searchHistoryArr[lastSearchedIndex];
+        getWeatherDetails(lastSearchedCity);
+        console.log(`Last searched city: ${lastSearchedCity}`);
+    }
+});
 
 locationButton.addEventListener("click", getUserCoordinates);
 searchButton.addEventListener("click", getCityCoordinates);
